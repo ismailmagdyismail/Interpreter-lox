c7056ebee187d077ca6b112f5cb7db60ad95fba6
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-09-05 23:49:28 +0300

    [Enhance]: add working_dir env variable , bin directory for build outputs

--- interpreter.sh
+++ interpreter.sh
@@ -1,10 +1,25 @@
 export project_name=interpreter
-export root_path=${PWD}
-export src_path=${root_path}/src
-export test_path=${root_path}/tests
-export bin_path=${root_path}/bin
+export working_dir=${PWD}
 
-function build(){
+export src_dir=src
+export src_path=${root_path}/${src_dir}
+
+export test_dir=tests
+export test_path=${root_path}/${test_dir}
+
+export bin_dir=bin
+export bin_path=${root_path}/${bin_dir}
+
+export build_dir=build
+export build_path=${root_path}/${build_dir}
+
+function init()
+{
+    mkdir ${build_dir} ${bin_dir}
+}
+
+function build()
+{
     echo Building
     mkdir build
     cd build
@@ -14,16 +29,18 @@ function build(){
     echo build finished
 }
 
-function run(){
+function run()
+{
     build
     echo Running
-    cd ${bin_path}
-    ./${project_name}
-    cd ..
+    cd ${working_dir}
+    ./${bin_dir}/${project_name}
+    cd ${working_dir}
 }
 
-function makeClean(){
-    cd build
+function makeClean()
+{
+    cd ${build_path}
     make clean
-    cd ..
+    cd..
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,4 @@
 int main(int argc, char **argv)
 {
     system("pwd");
-    std::unique_ptr<Logging::ILogger> logger = std::make_unique<FileLogger>(".logs/logs.logs");
-    logger->Error("Erorr message testing");
-    logger->Warn("Warning message testing");
-    logger->Log("Info message testing");
 }
