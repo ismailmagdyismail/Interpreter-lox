429be712c6ae86fec385bc3f41348c869fb5801c
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-09-07 18:10:47 +0300

    [Feature]: add token(s)

--- src/tokens/Token.cpp
+++ src/tokens/Token.cpp
@@ -1,1 +1,90 @@
 #include "tokens/Token.hpp"
+#include <optional>
+#include <unordered_map>
+#include <set>
+
+static std::set<std::string> keyWordsTokens = {
+    "and",
+    "or",
+    "class",
+    "true",
+    "false",
+    "fun",
+    "for",
+    "while",
+    "if",
+    "else",
+    "nil",
+    "print",
+    "return",
+    "super",
+    "this",
+    "var",
+    "eof"
+};
+
+static std::set<std::string> singleCharTokens = {
+    "(",
+    ")",
+    "{",
+    "}",
+    ",",
+    ".",
+    "-",
+    "+",
+    ";",
+    "/",
+    "*",
+    "!",
+    "=",
+    ">",
+    "<",
+};
+
+static std::set<std::string> twoCharTokens = {
+    "!=",
+    "==",
+    ">=",
+    "<=",
+};
+
+namespace Tokens
+{
+    bool isSingleCharToken(const std::string &lexeme)
+    {
+        return singleCharTokens.find(lexeme) != singleCharTokens.end();
+    }
+
+    bool isTwoCharToken(const std::string &lexeme)
+    {
+        return twoCharTokens.find(lexeme) != twoCharTokens.end();
+    }
+
+    bool isKeyword(const std::string &lexeme)
+    {
+        return keyWordsTokens.find(lexeme) != keyWordsTokens.end();
+    }
+
+    bool isReserved(const std::string &lexeme)
+    {
+        return isSingleCharToken(lexeme) || isTwoCharToken(lexeme) || isKeyword(lexeme);
+    }
+
+    std::optional<Token> createToken(const TokenType &tokenType,const std::string& lexeme,const unsigned int& lineNumber)
+    {
+        if(isReserved(lexeme))
+        {
+            return Token{
+                .tokenType = tokenType,
+                .lexeme = lexeme,
+                .lineNumber = lineNumber,
+            };
+        }
+        // TODO: check for valid identifiers / nums / strings
+        return Token{
+            .tokenType = tokenType,
+            .lexeme = lexeme,
+            .lineNumber = lineNumber,
+        };
+    }
+}
--- src/tokens/Token.hpp
+++ src/tokens/Token.hpp
@@ -7,13 +7,66 @@ namespace Tokens
 {
     enum class TokenType
     {
+        // Single-character tokens.
+        lEFT_PARNTHESES,  // (
+        RIGHT_PARNTHESES, // )
+        LEFT_BRACE,  // {
+        RIGHT_BRACE, // }
+        COMMA , // ,
+        DOT , // .
+        MINUS , // -
+        PLUS , // +
+        SEMI_COLON , // ;
+        SLASH , // /
+        STAR , // *
+        BANG , // !
+        ASSIGN, // =
+        GREATER , // >
+        SMALLER , // <
 
+
+        // two character tokens.
+        BANG_EQUAL , // !=
+        EQUAL , // ==
+        GREATER_OR_EQUAL , // >=
+        SMALLER_OR_EQUAL , // <=
+
+        // literals
+        IDENTFIER,
+        STRING,
+        NUMBER,
+
+        // keywords
+        AND,
+        OR,
+        CLASS,
+        TRUE,
+        FALSE,
+        FUN,
+        FOR,
+        WHILE,
+        IF,
+        ELSE,
+        NIL,
+        PRINT,
+        RETURN,
+        SUPER,
+        THIS,
+        VAR,
+        END_OF_FILE,
     };
     struct Token
     {
         TokenType tokenType;
+        unsigned int lineNumber;
+        std::string lexeme;
     };
 
-    std::string tokenLiteral(const Token& token);
-    std::optional<Token> createToken(const TokenType& tokenType);
+    // TODO: consider adding this as a generalization for is single , two char tokens to be able to easily add new tokens , be extenssible
+    // bool isStopWord(const std::string& lexeme);
+    bool isSingleCharToken(const std::string& lexeme);
+    bool isTwoCharToken(const std::string& lexeme);
+    bool isKeyword(const std::string& lexeme);
+    bool isReserved(const std::string& lexeme);
+    std::optional<Token> createToken(const TokenType& tokenType,std::string lexeme);
 }
