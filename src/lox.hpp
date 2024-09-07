3e61e9002890e2ef0307ca494fc9491d30492d0f
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-09-08 02:54:57 +0300

    [Fix]: delete copy constructor for SourceReprorter, move reported in lox constrcutor

--- src/lox.hpp
+++ src/lox.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-
 #include "lexer/Lexer.hpp"
 #include "sourceReporter/SourceReporter.hpp"
 #include "tokens/Token.hpp"
 #include <vector>
+
 class Lox
 {
 public:
-    Lox(const Lexer& lexer,const SourceReport::SourceReporter& reporter);
+    Lox(Lexer&& lexer,SourceReport::SourceReporter&& reporter);
     void runPipeline();
     std::vector<Tokens::Token> runLexerPhase();
 
