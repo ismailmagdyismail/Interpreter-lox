7d666c404a2c25564dbd5e8ecd216320ddae431f
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-09-19 20:24:01 +0300

    [ops]: remove install from commit.sh script

--- commit.sh
+++ commit.sh
@@ -14,19 +14,3 @@ function feature() {
     local commit_message=$1
     git commit -m "[Feature]: ${commit_message}"
 }
-
-
-function get_logger()
-{
-    cd ${working_dir}
-
-    rm -rf ${working_dir}/libs/
-
-    mkdir ${working_dir}/libs
-    mkdir ${lib_path}/includes
-    mkdir ${lib_path}/includes/logger
-    mkdir ${lib_path}/includes/debugger
-
-    cp -r ../../logger/logger/include/*.hpp ${lib_path}/includes/logger
-    cp ../../logger/logger/bin/liblogger.a ${lib_path}/
-}
